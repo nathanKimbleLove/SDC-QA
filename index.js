040a9c23e7e56e1b79968259fcfa131e83313cd2
Ubuntu <ubuntu@ip-172-31-42-79.us-east-2.compute.internal>
2023-02-22 20:18:50 +0000

    ready for today

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const reviewsRoute = require('./routes/reviews.js')
 
 // middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
+
 
 // routes
 app.use('/products', productsRoute)
 app.use('/qa', qaRoute)
 app.use('/reviews', reviewsRoute)
+
+app.get('/loader*', (req, res) => {
+  res.send('loaderio-f56cfcb23bb3ac3b1d20a6b1983f4cea')
+})
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
