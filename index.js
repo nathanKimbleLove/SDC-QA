40e0f8d1aa65964a5061e77926318e0d3e6dd384
Ubuntu <ubuntu@ip-172-31-41-244.us-east-2.compute.internal>
2023-02-22 19:52:39 +0000

    configured for aws

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 const productsRoute = require('./routes/products.js')
 const qaRoute = require('./routes/qa.js')
@@ -8,6 +9,7 @@ const reviewsRoute = require('./routes/reviews.js')
 
 // middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
 // routes
 app.use('/products', productsRoute)
